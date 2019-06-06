7ce63fe30419814631ef49f4357e3d6e10c36db4
Marek Niedbach <marek.niedbach@silvair.com>
2019-06-06 16:02:47 +0200

    Add setup requires

--- MobiliumClient/setup.py
+++ MobiliumClient/setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='mobilium-client',
+setup(name='mobilium_client',
       version='0.0.1',
       description='Mobilium Client',
       url='https://github.com/homersoft/Mobilium',
       license='MIT',
       packages=find_packages(),
+      setup_requires=[
+          'pytest-runner'
+      ],
       tests_require=[
           'pytest'
       ],
--- MobiliumServer/setup.py
+++ MobiliumServer/setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='mobilium-server',
+setup(name='mobilium_server',
       version='0.0.1',
       description='Mobilium Server',
       url='https://github.com/homersoft/Mobilium',
       license='MIT',
       packages=find_packages(),
+      setup_requires=[
+          'pytest-runner'
+      ],
       tests_require=[
           'pytest'
       ],
