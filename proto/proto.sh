f52657a5b3323032543fd92ba3aa7cbcd99aa7c9
Tomasz Oraczewski <tomasz.oraczewski@silvair.com>
2019-07-31 10:37:00 +0200

    Fix proto script paths

--- proto/proto.sh
+++ proto/proto.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-messages_file=proto/messages.proto
-messages_module=MobiliumProtoMessages/mobilium_proto_messages
-driver=MobiliumDriver/MobiliumDriver
+messages_file=messages.proto
+messages_module=../MobiliumProtoMessages/mobilium_proto_messages/proto
+driver=../MobiliumDriver/MobiliumDriver/proto
 protoc --python_out=$messages_module --swift_out=$driver $messages_file
