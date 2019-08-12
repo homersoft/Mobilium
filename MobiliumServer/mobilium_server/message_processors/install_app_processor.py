555b12501658141911616cc6b06d530031863a78
Tomasz Oraczewski <tomasz.oraczewski@silvair.com>
2019-08-12 09:35:51 +0200

    Pass app parameters in messages from client

--- MobiliumServer/mobilium_server/message_processors/install_app_processor.py
+++ MobiliumServer/mobilium_server/message_processors/install_app_processor.py
@@ -6,15 +6,13 @@ from mobilium_server.shell_executor import ShellExecutor
 
 class InstallAppProcessor(MessageProcessor):
 
-    IPA_PATH = './app.ipa'
-
     async def _process(self, data: bytes):
         message = MessageDeserializer.install_app_request(data)
         if message is not None:
-            await self.install_app(message.udid)
+            await self.install_app(message.udid, message.file_path)
 
-    async def install_app(self, udid: str):
-        command = 'ideviceinstaller -u {0} -i {1}'.format(udid, InstallAppProcessor.IPA_PATH)
+    async def install_app(self, udid: str, file_path: str):
+        command = 'ideviceinstaller -u {0} -i {1}'.format(udid, file_path)
         ShellExecutor.execute(command)
         message = MessageDataFactory.install_app_response()
         await self.message_sender.send(message)
