98f0d90ae55752641443e2737008a130767d0327
Mateusz Mularski <mateusz.mularski@silvair.com>
2020-01-15 11:08:53 +0100

    (F)[SP-6590] Run 'carthage update' command before building driver
    * Run 'carthage update' command before building driver
    
    * Add prepare driver message
    
    * Apply PR suggestions
    
    * minor cleaning
    
    * Apply PR suggestions [2]

--- MobiliumServer/mobilium_server/message_processors/start_driver_processor.py
+++ MobiliumServer/mobilium_server/message_processors/start_driver_processor.py
@@ -1,4 +1,3 @@
-from os import path
 from typing import Optional
 
 from mobilium_proto_messages.message_deserializer import MessageDeserializer
@@ -6,13 +5,10 @@ from mobilium_proto_messages.message_sender import MessageSender
 from mobilium_proto_messages.message_processor import MessageProcessor
 from mobilium_server.utils.shell_executor import ShellExecutor
 from mobilium_server.message_processors.shell_message_processor import ShellMessageProcessor
+from mobilium_server.utils.mobilium_driver_localization import get_project_dir, PROJECT_NAME, SCHEME
 
 
 class StartDriverProcessor(ShellMessageProcessor):
-
-    INTERNAL_PROJECT_PATH = '../MobiliumDriver/MobiliumDriver.xcodeproj'
-    EXTERNAL_PROJECT_PATH = 'Mobilium/MobiliumDriver/MobiliumDriver.xcodeproj'
-    SCHEME = 'MobiliumDriver'
 
     def __init__(self, shell_executor: ShellExecutor, message_sender: MessageSender, address: str, port: int,
                  successor: Optional[MessageProcessor] = None):
@@ -26,11 +22,7 @@ class StartDriverProcessor(ShellMessageProcessor):
             self.start_driver(message.udid)
 
     def start_driver(self, udid: str):
-        command = 'xcodebuild -project {0} -scheme {1} -destination "platform=iOS,id={2}" HOST={3} PORT={4} test' \
-            .format(self.project_path(), StartDriverProcessor.SCHEME, udid, self.address, self.port)
-        self.shell_executor.execute(command, track_output=True, waits_for_termination=False)
-
-    def project_path(self):
-        if path.exists(self.INTERNAL_PROJECT_PATH):
-            return self.INTERNAL_PROJECT_PATH
-        return self.EXTERNAL_PROJECT_PATH
+        project_dir = get_project_dir()
+        build_command = 'xcodebuild -project {0} -scheme {1} -destination "platform=iOS,id={2}" HOST={3} PORT={4} test'\
+            .format(project_dir + PROJECT_NAME, SCHEME, udid, self.address, self.port)
+        self.shell_executor.execute(build_command, track_output=True, waits_for_termination=False)
