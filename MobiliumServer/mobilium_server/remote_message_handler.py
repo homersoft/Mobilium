8ec409e1455eb2b5ff7492f6adc73efe7c6e275d
Robert Pajor <robert.pajor@silvair.com>
2020-01-29 15:33:36 +0100

    (F)[SP-6607] Add waiting for reconnect in Mobilium client and server
    * Add waiting for reconnect in Mobilium client and server
    
    * Review changes for wait_until_true function

--- MobiliumServer/mobilium_server/remote_message_handler.py
+++ MobiliumServer/mobilium_server/remote_message_handler.py
@@ -9,12 +9,15 @@ class RemoteMessageHandler(MessageHandler, AsyncNamespace):
     def __init__(self, name: str):
         MessageHandler.__init__(self, name)
         AsyncNamespace.__init__(self, name)
+        self.is_connected = None
 
     def on_connect(self, sid, environ):
         print('{0} connected'.format(self.name))
+        self.is_connected = True
 
     def on_disconnect(self, sid):
         print('{0} disconnected'.format(self.name))
+        self.is_connected = False
 
     async def process_message(self, data: bytes):
         print('{0} << {1}'.format(self.name, data))
