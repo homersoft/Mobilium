ab8fc9a96e7f6cddea6604d83237a1bffc55f0b2
Robert Pajor <robert.pajor@silvair.com>
2020-02-05 10:27:47 +0100

    (F)[SP-6607] Add checking for connection to Mobilium Driver
    * Add checking for connection to Mobilium Driver
    
    * Add message to XCTFail

--- MobiliumDriver/MobiliumDriver/Utilities/SocketIOClient.swift
+++ MobiliumDriver/MobiliumDriver/Utilities/SocketIOClient.swift
@@ -8,9 +8,36 @@
 
 import Foundation
 import SocketIO
+import XCTest
 
 extension SocketIOClient {
     func send(message data: Data) {
-        emit("message", with: [data])
+        let deadline = Date(timeIntervalSinceNow: 30)
+        waitForConnection(until: deadline, success: { [weak self] in
+            self?.emit("message", with: [data])
+        }, failure: { [weak self] in
+            self?.failDriver()
+        })
+    }
+
+    private func waitForConnection(until deadline: Date, success: @escaping () -> (), failure: @escaping () ->()) {
+        if isConnected() {
+            success()
+        } else if deadline.timeIntervalSinceNow < 0 {
+            failure()
+        } else {
+            print("Driver is waiting for connection with server...")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+                self?.waitForConnection(until: deadline, success: success, failure: failure)
+            }
+        }
+    }
+
+    private func isConnected() -> Bool {
+        return self.status == .connected
+    }
+
+    private func failDriver() {
+        XCTFail("Driver reconnection failed!")
     }
 }
